68e28395e7df2d244e87df51fdffc002c66be6d7
SVTA <117646988+svta-7125@users.noreply.github.com>
2023-05-22 19:44:39 +0530

    Add files via upload

---
+++ hehe.py
@@ -0,0 +1,38 @@
+import os, time, sys
+try : import pyautogui
+except: os.system('pip install pyautogui')
+def s():
+    time.sleep(1)
+    os.system('cls')
+def scroller(x):
+    print(x, end="")
+    a = '.....................................'
+    for char in a:  
+        sys.stdout.write(char)
+        sys.stdout.flush()  
+        time.sleep(0.06)
+    print('Finished')
+os.system('color 0A')
+os.system('cls')
+pyautogui.press('f11')
+print("HAHAHAHAH UR PC IS NOW HACKKED!!!!")
+print("\nAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+time.sleep(2)
+scroller("Deleting system32(its the folder where all critical windows files are stored.)")
+time.sleep(1.5)
+os.system('cls')
+print("Muahahaahhahaahaha")
+s()
+print("Self destruct in 5")
+s()
+print("Self destruct in 4")
+s()
+print('Self destruct in 3')
+s()
+print('Self destruct in 2')
+s()
+print('Self destruct in 1')
+s()
+print('UR PC IS NOW DED!!!')
+time.sleep(3)
+os.system('shutdown -p')
